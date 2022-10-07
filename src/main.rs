36262d2e8f5b7fe7f282b7191822d50238bb61eb
brkp <brkp@tutanota.com>
2022-10-07 13:45:40 +0300

    disable warnings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+#![allow(
+    dead_code,
+    unused_imports,
+    unused_variables)]
+
 mod states;
+mod renderer;
 
 fn main() {
-    arche::ContextBuilder::new("software renderer".to_owned(), 800, 600)
+    arche::ContextBuilder::new("software renderer".to_owned(), 640, 480)
         .vsync(false)
-        .grab_mouse(true)
-        .show_mouse(false)
+        // .grab_mouse(true)
+        // .show_mouse(false)
         .build()
         .unwrap()
         .run(|ctx| states::Main::new(ctx));
