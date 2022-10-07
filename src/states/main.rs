e08b33d641dc813a35d1d1afb08ba5d8461cf9c2
brkp <brkp@tutanota.com>
2022-10-07 13:45:27 +0300

    main state

--- src/states/main.rs
+++ src/states/main.rs
@@ -2,15 +2,26 @@ use arche::prelude::*;
 use arche::winit::event::VirtualKeyCode;
 use arche::winit_input_helper::WinitInputHelper;
 
-pub struct MainState;
+use crate::renderer::scene::Scene;
+
+pub struct MainState {
+    scene: Scene,
+}
 
 impl MainState {
-    pub fn new(_ctx: &mut Context) -> Box<dyn State> {
-        Box::new(MainState {})
+    pub fn new(ctx: &mut Context) -> Box<dyn State> {
+        Box::new(MainState {
+            scene: Scene::new(ctx),
+        })
     }
 }
 
 impl State for MainState {
+    fn on_start(&mut self, ctx: &mut Context) {
+        self.scene
+            .push_object(obj::Obj::load("assets/teapot.obj").unwrap());
+    }
+
     fn handle_events(&mut self, _ctx: &mut Context, input: &WinitInputHelper) -> Trans {
         if input.quit() || input.key_pressed(VirtualKeyCode::Escape) {
             return Trans::Quit;
@@ -23,5 +34,7 @@ impl State for MainState {
 
     fn draw(&mut self, ctx: &mut Context) {
         draw::fill(ctx, Color::rgb(0x6495ed));
+
+        self.scene.render(ctx);
     }
 }
