295fc7861179c90e859e3f3fe0ef8bcb45ea2c47
brkp <brkp@tutanota.com>
2022-10-07 14:40:57 +0300

    `Vec3` implementation

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,2 +1,3 @@
-pub(crate) mod scene;
 pub(crate) mod camera;
+pub(crate) mod scene;
+pub(crate) mod vec3;
---
+++ src/renderer/vec3.rs
@@ -0,0 +1,51 @@
+use std::convert::From;
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl From<[f32; 3]> for Vec3 {
+    fn from(v: [f32; 3]) -> Self {
+        Self::new(v[0], v[1], v[2])
+    }
+}
+
+impl Into<[f32; 3]> for Vec3 {
+    fn into(self) -> [f32; 3] {
+        [self.x, self.y, self.z]
+    }
+}
